004d3ea8876abdab2fbe7e22274a150b2e844bc4
blackarmVR <honungsbjorn@protonmail.com>
2022-04-22 14:45:07 +0100

    all sorting working

--- code.js
+++ code.js
@@ -56,107 +56,48 @@ var testValidRackMounted = function (hardwareSysId, tempHardwareData, tempModelD
     // all tests passed
     return true;
 };
-// const testValidPatchpanel = (
-//   patchpanel: Patchpanel,
-//   modelData: Record<string, Model>,
-// ) => {
-//   try {
-//     if (patchpanel.patchRackU === null) {
-//       return {
-//         pass: false,
-//         failReport: 'not a valid  patchpanel - u_rack_u is missing',
-//       };
-//     }
-//     if (patchpanel.patchRackU === 0) {
-//       return {
-//         pass: false,
-//         failReport: 'not a valid  patchpanel - u_rack_u is zero',
-//       };
-//     }
-//     if (patchpanel.patchModelSysId === null) {
-//       return {
-//         pass: false,
-//         failReport: 'not a valid patchpanel - does not have a model',
-//       };
-//     }
-//     if (!hasKey(modelData, patchpanel.patchModelSysId)) {
-//       return {
-//         pass: false,
-//         failReport: 'not a valid patchpanel - model not found',
-//       };
-//     }
-//     const model: Model = modelData[patchpanel.patchModelSysId];
-//     if (model.rackUnits === null) {
-//       return {
-//         pass: false,
-//         failReport: 'not a valid patchpanel - model height missing',
-//       };
-//     }
-//     if (model.rackUnits < 1) {
-//       return {
-//         pass: false,
-//         failReport: 'not a valid patchpanel - model height is less than 1',
-//       };
-//     }
-//     return {
-//       pass: true,
-//       failReport: '',
-//     };
-//   } catch (err) {
-//     errorLog('testValidPatchpanel', err);
-//     return {
-//       pass: false,
-//       failReport: 'not a valid patchpanel - function crashed',
-//     };
-//   }
-// };
-// const testValidChassisNetwork = (
-//   hardware: Hardware,
-//   hardwareData: Record<string, Hardware>,
-// ) => {
-//   try {
-//     if (hardware.parent === null) {
-//       return {
-//         pass: false,
-//         failReport: 'not valid network gear - no parent',
-//       };
-//     }
-//     if (!hasKey(hardwareData, hardware.parent)) {
-//       return {
-//         pass: false,
-//         failReport: 'not valid network gear - parent not found in hardwareData',
-//       };
-//     }
-//     if (hardwareData[hardware.parent].rackSysId !== hardware.rackSysId) {
-//       return {
-//         pass: false,
-//         failReport: 'not valid network gear - parent not in the same rack',
-//       };
-//     }
-//     if (hardware.modelCategoryName !== 'Network Gear') {
-//       return {
-//         pass: false,
-//         failReport: 'not valid network gear - model category is not network gear',
-//       };
-//     }
-//     return {
-//       pass: true,
-//       failReport: '',
-//     };
-//   } catch (err) {
-//     errorLog('testValidChassisNetwork', err);
-//     return {
-//       pass: false,
-//       failReport: 'not valid network gear - function crashed',
-//     };
-//   }
-// };
+var testValidNetworkGear = function (hardwareSysId, tempHardwareData) {
+    var tempHardware = tempHardwareData[hardwareSysId];
+    // needs a parent sys_id
+    if (tempHardware.parent === null) {
+        return false;
+    }
+    // parent must exist
+    if (!hasKey(tempHardwareData, tempHardware.parent)) {
+        return false;
+    }
+    // parent must be in the same rack
+    if (tempHardwareData[tempHardware.parent].rackSysId !== tempHardware.rackSysId) {
+        return false;
+    }
+    // model category must be correct
+    if (tempHardware.modelCategoryName !== 'Network Gear') {
+        return false;
+    }
+    // all tests passed
+    return true;
+};
+var testValidPdu = function (hardwareSysId, tempHardwareData) {
+    var tempHardware = tempHardwareData[hardwareSysId];
+    // model category must be correct
+    if (tempHardware.modelCategoryName !== 'PDU') {
+        return false;
+    }
+    // all tests passed
+    return true;
+};
 var findCategory = function (hardwareSysId, tempHardwareData, tempModelData) {
     if (testValidChassisSled(hardwareSysId, tempHardwareData)) {
         return 'sled';
     }
     if (testValidRackMounted(hardwareSysId, tempHardwareData, tempModelData)) {
         return 'rack_mounted';
+    }
+    if (testValidNetworkGear(hardwareSysId, tempHardwareData)) {
+        return 'network_gear';
+    }
+    if (testValidPdu(hardwareSysId, tempHardwareData)) {
+        return 'pdu';
     }
     return 'bad_data';
 };
@@ -171,17 +112,26 @@ var sortHardware = function (tempHardwareData, tempModelData) {
             if (!hasKey(outputData, tempRackName)) {
                 outputData[tempRackName] = {
                     'bad_data': {},
-                    'network': {},
+                    'network_gear': {},
                     'pdu': {},
                     'rack_mounted': {},
                     'sled': {},
                 };
             }
-            if (category === 'sled') {
-                outputData[tempRackName].sled[hardwareSysId] = tempHardwareData[hardwareSysId];
+            if (category === 'bad_data') {
+                outputData[tempRackName].bad_data[hardwareSysId] = tempHardwareData[hardwareSysId];
+            }
+            if (category === 'network_gear') {
+                outputData[tempRackName].network_gear[hardwareSysId] = tempHardwareData[hardwareSysId];
+            }
+            if (category === 'pdu') {
+                outputData[tempRackName].pdu[hardwareSysId] = tempHardwareData[hardwareSysId];
             }
             if (category === 'rack_mounted') {
                 outputData[tempRackName].rack_mounted[hardwareSysId] = tempHardwareData[hardwareSysId];
+            }
+            if (category === 'sled') {
+                outputData[tempRackName].sled[hardwareSysId] = tempHardwareData[hardwareSysId];
             }
         }
     });
@@ -293,6 +243,6 @@ var main = function (tempRackSysIdList) {
     sortHardware(hardwareData, modelData);
 };
 //
-var rackSysIdString = 'bc22df4adb1ec70cab79f7d41d9619f6,b817db4edb168bc010b6f1561d961914,f4738c21dbb1c7442b56541adc96196a,b1c34461dbb1c7442b56541adc96198f,efd3cc61dbb1c7442b56541adc961978,bdba2b74db271788259e5898dc9619a4,3abaa3f4db271788259e5898dc9619ab,3bba63f4db271788259e5898dc961971,30cae3f4db271788259e5898dc961926,0aca67f4db271788259e5898dc961979,e3a4fc5bdb7f8b80a9885205dc9619a5,3eca67f4db271788259e5898dc961980,3bca27f4db271788259e5898dc9619a1,09da2bf4db271788259e5898dc961954,c2da63f4db271788259e5898dc96197a,03da2bf4db271788259e5898dc961946,40c4789bdb7f8b80a9885205dc9619d8,9dec376a2b45820054a41bc5a8da15e9,c2ea8b2edb151f80a9885205dc9619d9,fa94f8ebdbed389459ac6e25ca9619fe,acbc736a2b45820054a41bc5a8da1503';
+var rackSysIdString = 'c2ea8b2edb151f80a9885205dc9619d9,3abaa3f4db271788259e5898dc9619ab';
 var rackSysIdList = rackSysIdString.split(',');
 main(rackSysIdList);
