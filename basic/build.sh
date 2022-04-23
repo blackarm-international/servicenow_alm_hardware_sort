622304bc30c1628fd9e4520c7bdd7c8c4ea41219
blackarmVR <honungsbjorn@protonmail.com>
2022-04-23 10:19:32 +0100

    moved files

---
+++ basic/build.sh
@@ -0,0 +1,18 @@
+echo
+echo "checking basic.ts with airbnb eslint"
+if npx eslint basic.ts
+then
+  echo "done"
+else
+  exit
+fi
+echo
+#
+echo "transpiling basic.ts"
+if tsc
+then
+  echo "done"
+else
+  exit
+fi
+echo
